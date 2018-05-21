4fa22c66d91f2e059757389f4653df66ac383539
sdlfkorg <sdlfkorg@gmail.com>
2018-05-21 22:10:25 +0800

    fix chart size

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 export default (props) => {
+	
 	return (
 		<div>
-			<Sparklines height={120} width={180} data={props.data}>
+			<Sparklines svgHeight={120} svgWidth={180} data={props.data}>
 			 <SparklinesLine color={props.color} />
 			</Sparklines>
 		</div>
